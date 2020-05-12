5053db314d72d48ea68cb52c09900bfe598b0f13
Mateus-Carmo31 <mateus_oliveira31@outlook.com>
2020-05-11 22:18:22 -0300

    Added weapon sprites. Working on a better way to do attacks.

--- GWJ21/src/Scripts/MeleeEnemy.gd
+++ GWJ21/src/Scripts/MeleeEnemy.gd
@@ -7,6 +7,10 @@ export(float) var attack_cooldown = 1.0
 export(float, 50, 100, 1) var min_attack_dist := 50.0
 
 var state = APPROACH
+var hitbox_on : bool = false
+
+onready var weapon_holding_slot_L = $Weapon.position
+onready var weapon_holding_slot_R = Vector2(-weapon_holding_slot_L.x, $Weapon.position.y)
 
 func _ready():
 	._ready()
@@ -41,6 +45,13 @@ func approach_state(delta):
 	if current_player:
 		var target_dir = global_position.direction_to(get_node(current_player).global_position)
 		velocity = lerp(velocity, target_dir * max_speed, move_speed * delta)
+		
+		if velocity.x > 0:
+			$Sprite.flip_h = true
+			$Weapon.position = lerp($Weapon.position, weapon_holding_slot_R, delta * 5.0)
+		else:
+			$Sprite.flip_h = false
+			$Weapon.position = lerp($Weapon.position, weapon_holding_slot_L, delta * 5.0)
 	
 	velocity = move_and_slide(velocity)
 	
@@ -61,8 +72,27 @@ func approach_state(delta):
 func attack_state(delta):
 	if current_player:
 		$AttackPivot.look_at(get_node(current_player).global_position)
+		$Weapon.position = lerp($Weapon.position, $AttackPivot.position, delta * 5.0)
+		$Weapon.rotation_degrees = lerp($Weapon.rotation_degrees,
+										$AttackPivot.rotation_degrees + 45,
+										delta * 5)
+
+func dazed_state(delta):
+	
+	daze = max(daze - recovery_speed * delta, 0)
+	
+	if daze == 0:
+		print(name, "'s not dazed anymore!")
+		state = APPROACH
 
 func attack():
+	
+	$Tween.interpolate_property($Weapon, "rotation_degrees",
+								$Weapon.rotation_degrees, 
+								$Weapon.rotation_degrees+90,
+								0.1, Tween.TRANS_CUBIC, Tween.EASE_IN)
+	$Tween.start()
+	
 	$StateSwitchTimer.disconnect("timeout", self, "attack")
 	var bodies = $AttackPivot/AttackHitbox.get_overlapping_bodies()
 	
@@ -80,11 +110,3 @@ func select():
 func take_damage(damage_taken : int):
 	.take_damage(damage_taken)
 	add_daze(max_daze)
-
-func dazed_state(delta):
-	
-	daze = max(daze - recovery_speed * delta, 0)
-	
-	if daze == 0:
-		print(name, "'s not dazed anymore!")
-		state = APPROACH
