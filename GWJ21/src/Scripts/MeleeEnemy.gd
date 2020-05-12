55ea8f234100a39dc66909129455f8d82df8b4ae
Mateus-Carmo31 <mateus_oliveira31@outlook.com>
2020-05-11 23:01:05 -0300

    Worked more on the better attack system. There is a problem related to lerping rotations happening. Try to fix later!

--- GWJ21/src/Scripts/MeleeEnemy.gd
+++ GWJ21/src/Scripts/MeleeEnemy.gd
@@ -9,14 +9,15 @@ export(float, 50, 100, 1) var min_attack_dist := 50.0
 var state = APPROACH
 var hitbox_on : bool = false
 
+var current_facing = true # LEFT => TRUE | RIGHT => FALSE
 onready var weapon_holding_slot_L = $Weapon.position
 onready var weapon_holding_slot_R = Vector2(-weapon_holding_slot_L.x, $Weapon.position.y)
 
 func _ready():
 	._ready()
 	
-	$AttackPivot/AttackHitbox.collision_layer = LayerManager.LAYERS.MISC
-	$AttackPivot/AttackHitbox.collision_mask = LayerManager.LAYERS.PLAYER
+	$Weapon/AttackHitbox.collision_layer = LayerManager.LAYERS.MISC
+	$Weapon/AttackHitbox.collision_mask = LayerManager.LAYERS.PLAYER
 
 func _on_collision(collided_with : KinematicSelectable):
 	._on_collision(collided_with)
@@ -33,7 +34,7 @@ func _physics_process(delta):
 			ATTACK:
 				attack_state(delta)
 			COOLDOWN:
-				pass
+				cooldown_state(delta)
 	else:
 		dazed_state(delta)
 
@@ -46,12 +47,8 @@ func approach_state(delta):
 		var target_dir = global_position.direction_to(get_node(current_player).global_position)
 		velocity = lerp(velocity, target_dir * max_speed, move_speed * delta)
 		
-		if velocity.x > 0:
-			$Sprite.flip_h = true
-			$Weapon.position = lerp($Weapon.position, weapon_holding_slot_R, delta * 5.0)
-		else:
-			$Sprite.flip_h = false
-			$Weapon.position = lerp($Weapon.position, weapon_holding_slot_L, delta * 5.0)
+		current_facing = velocity.x < 0
+		update_facing(delta)
 	
 	velocity = move_and_slide(velocity)
 	
@@ -66,16 +63,38 @@ func approach_state(delta):
 			print("Reached!")
 			velocity = Vector2.ZERO
 			state = ATTACK
-			$StateSwitchTimer.start(attack_telegraph)
 			$StateSwitchTimer.connect("timeout", self, "attack")
+			$StateSwitchTimer.start(attack_telegraph)
 
 func attack_state(delta):
 	if current_player:
 		$AttackPivot.look_at(get_node(current_player).global_position)
 		$Weapon.position = lerp($Weapon.position, $AttackPivot.position, delta * 5.0)
-		$Weapon.rotation_degrees = lerp($Weapon.rotation_degrees,
-										$AttackPivot.rotation_degrees + 45,
-										delta * 5)
+		$Weapon.rotation = lerp($Weapon.rotation,
+								$AttackPivot.rotation + PI/4.0,
+								delta * 5)
+
+func attack():
+	
+	$StateSwitchTimer.disconnect("timeout", self, "attack")
+	
+	hitbox_on = true
+	$Tween.interpolate_property($Weapon, "rotation",
+								$Weapon.rotation,
+								$Weapon.rotation+PI/2.0,
+								0.1, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	
+	$Tween.start()
+
+func end_attack():
+	hitbox_on = false
+	state = COOLDOWN
+	$StateSwitchTimer.connect("timeout", self, "set", ["state", APPROACH])
+	$StateSwitchTimer.start(attack_cooldown)
+
+func cooldown_state(delta):
+	$Weapon.rotation = lerp($Weapon.rotation, 0, delta * 5)
+	update_facing(delta)
 
 func dazed_state(delta):
 	
@@ -85,23 +104,13 @@ func dazed_state(delta):
 		print(name, "'s not dazed anymore!")
 		state = APPROACH
 
-func attack():
-	
-	$Tween.interpolate_property($Weapon, "rotation_degrees",
-								$Weapon.rotation_degrees, 
-								$Weapon.rotation_degrees+90,
-								0.1, Tween.TRANS_CUBIC, Tween.EASE_IN)
-	$Tween.start()
-	
-	$StateSwitchTimer.disconnect("timeout", self, "attack")
-	var bodies = $AttackPivot/AttackHitbox.get_overlapping_bodies()
-	
-	if bodies.has(get_node(current_player)):
-		print("Hit!")
-	
-	state = COOLDOWN
-	$StateSwitchTimer.connect("timeout", self, "set", ["state", APPROACH])
-	$StateSwitchTimer.start(attack_cooldown)
+func update_facing(delta):
+	if current_facing == true:
+		$Sprite.flip_h = false
+		$Weapon.position = lerp($Weapon.position, weapon_holding_slot_L, delta * 5)
+	else:
+		$Sprite.flip_h = true
+		$Weapon.position = lerp($Weapon.position, weapon_holding_slot_R, delta * 5)
 
 func select():
 	.select()
@@ -110,3 +119,7 @@ func select():
 func take_damage(damage_taken : int):
 	.take_damage(damage_taken)
 	add_daze(max_daze)
+
+func _on_AttackHitbox_body_entered(body):
+	if hitbox_on:
+		print("Hit ", body.name)
