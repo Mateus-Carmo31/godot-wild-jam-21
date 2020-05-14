3fcd574269a9afb884685f936427904ad354e6b3
Mateus-Carmo31 <mateus_oliveira31@outlook.com>
2020-05-14 01:40:10 -0300

    Added the blockades. Need to polish them more, but they seem to be working quite well. The player can still dodge them if they're cheeky.

--- GWJ21/src/Scripts/Blockade.gd
+++ GWJ21/src/Scripts/Blockade.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
+var is_locked := false
+
 func _ready():
 	
 	collision_layer = LayerManager.LAYERS.WALLS
@@ -7,9 +9,34 @@ func _ready():
 		LayerManager.LAYERS.CONNECTION_HITBOX,
 		LayerManager.LAYERS.MISC
 	])
+	
+	hide()
 
 func lock():
-	pass
+	if $Tween.is_connected("tween_all_completed", self, "hide"):
+		$Tween.disconnect("tween_all_completed", self, "hide")
+	
+	show()
+	$CollisionShape2D.set_deferred("disabled", false)
+	$Tween.interpolate_property($Lock, "scale", Vector2.ZERO, Vector2(4,4), 0.2,
+								Tween.TRANS_BACK, Tween.EASE_OUT)
+	$Tween.interpolate_property($Left, "scale:x", 0, 4, 0.5,
+								Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	$Tween.interpolate_property($Right, "scale:x", 0, 4, 0.5,
+								Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	
+	$Tween.start()
 
 func unlock():
-	pass
+	$CollisionShape2D.set_deferred("disabled", true)
+	$Tween.interpolate_property($Lock, "scale", Vector2(4,4), Vector2.ZERO, 0.2,
+								Tween.TRANS_BACK, Tween.EASE_OUT)
+	$Tween.interpolate_property($Left, "scale:x", 4, 0, 0.5,
+								Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	$Tween.interpolate_property($Right, "scale:x", 4, 0, 0.5,
+								Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	
+	if not $Tween.is_connected("tween_all_completed", self, "hide"):
+		$Tween.connect("tween_all_completed", self, "hide")
+	
+	$Tween.start()
--- GWJ21/src/Scripts/Room.gd
+++ GWJ21/src/Scripts/Room.gd
@@ -2,6 +2,7 @@ extends Area2D
 class_name Room
 
 export(float) var cam_zoom = 1.0
+export(Array, NodePath) var blockades = []
 
 var current_enemies = []
 var is_completed : bool = false
@@ -26,6 +27,8 @@ func _on_Room_body_entered(_body):
 		for child in get_children():
 			if child is Spawner:
 				child.call_deferred("spawn", _body.get_path())
+		
+		lock_doors(0.5)
 
 func _on_Room_body_exited(_body):
 	Events.emit_signal("released_cam_focus")
@@ -39,14 +42,36 @@ func deregister_enemy(enemy):
 	current_enemies.erase(enemy)
 	
 	if current_enemies.size() == 0:
-		Events.disconnect("enemy_spawned", self, "register_enemy")
-		Events.disconnect("enemy_died", self, "deregister_enemy")
-		is_completed = true
-		print("Room completed!")
+		complete_room()
 	else:
 		print(current_enemies.size(), " remaining!")
+
+func complete_room():
+	Events.disconnect("enemy_spawned", self, "register_enemy")
+	Events.disconnect("enemy_died", self, "deregister_enemy")
+	is_completed = true
+	
+	unlock_doors()
+	
+	print("Room completed!")
 
 func empty_room():
 	for child in get_children():
 		if child is Spawner:
 			child.delete_spawned_object()
+	
+	unlock_doors()
+
+func lock_doors(wait = -1):
+	if wait != -1:
+		yield(get_tree().create_timer(wait), "timeout")
+	
+	for blockade in blockades:
+		get_node(blockade).lock()
+
+func unlock_doors(wait = -1):
+	if wait != -1:
+		yield(get_tree().create_timer(wait), "timeout")
+	
+	for blockade in blockades:
+		get_node(blockade).unlock()
